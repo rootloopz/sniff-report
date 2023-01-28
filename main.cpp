79b20ad0876f92531acc0f5dff96d96a11a700d7
ydoco <cwillard1@mail.csuchico.edu>
2023-01-28 15:59:23 -0800

    simplifed setup and packet parsing functions

--- main.cpp
+++ main.cpp
@@ -2,13 +2,13 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
-#include <fstream>
 #include <tins/packet.h>
+#include <tins/sniffer.h>
 
 using namespace std;
 using namespace Tins;
 
-//needed to get MAC addrs
+// needed to get MAC addrs
 template <class T> HWAddress<6> get_src_addr(const T &data) {
   if (!data.from_ds() && !data.to_ds())
     return data.addr2();
@@ -17,90 +17,40 @@ template <class T> HWAddress<6> get_src_addr(const T &data) {
   return data.addr3();
 }
 
-string partial_parse_radiotap(const Packet &packet) {
-  stringstream ss;
-
-  try {
-    const RadioTap &rt = packet.pdu()->rfind_pdu<RadioTap>();
-
-    try {
-      unsigned int timestamp = packet.timestamp().seconds();
-      ss << "\"timestamp\":" << timestamp;
-    } catch (...) {
-    }
-
-    try {
-      int dbm_signal = rt.dbm_signal();
-      ss << ",\"dbm_signal\":" << dbm_signal;
-    } catch (...) {
-    }
-
-    try {
-      int rate = rt.rate();
-      ss << ",\"rate\":" << rate;
-    } catch (...) {
-    }
-
-    try {
-      int channel_freq = rt.channel_freq();
-      ss << ",\"channel_freq\":" << channel_freq;
-    } catch (...) {
-    }
-
-  } catch (...) {
-  }
-
-  return ss.str();
-}
-
 string parse_probe(const Packet &packet) {
-  stringstream ss;
-  try {
-    // first check that it's a probe request frame
-    packet.pdu()->rfind_pdu<Tins::Dot11ProbeRequest>();
-    const Dot11ManagementFrame &data = packet.pdu()->rfind_pdu<Dot11ManagementFrame>();
-    string ssid = data.ssid();
-    if(!ssid.empty()){
-      ssid = data.ssid();
-    } else {
-      ssid = "NULL";
-    }
-    HWAddress<6> src_addr = get_src_addr(data);
-      ss << "{"
-      << partial_parse_radiotap(packet) << ","
-      << "\"src_addr\":\"" << src_addr << "\","
-      << "\"ssid\":\"" << ssid << "\""
-      << "}\n";
-  } catch (...) {
-  }
-  return ss.str();
-}
-
-
-bool print0 = false;
-string println(string str) {
-  string result = "";
-    if(str.size() > 0) {
-        result += str;
-        if(print0) {
-            result += '\0';
-        } else {
-            result += '\n';
+    stringstream ss;
+    try {
+        // first check that it's a probe request frame
+        if (const auto *probe = packet.pdu()->find_pdu<Dot11ProbeRequest>()) {
+            string ssid = probe->ssid();
+            if (ssid.empty()) {
+                ssid = "NULL";
+            }
+            ss << "timestamp:" << packet.timestamp().seconds()
+               << " src_addr:" << probe->addr2()
+               << " ssid:" << ssid;
+            if (const auto *rt = packet.pdu()->find_pdu<RadioTap>()) {
+                ss << " dbm_signal:" << (int)rt->dbm_signal()
+                   << " rate:" << (int)rt->rate()
+                   << " channel_freq:" << rt->channel_freq();
+            }
+            ss << endl;
         }
-        cout.flush();
+    } catch (...) {
     }
-    return result;
+    return ss.str();
 }
 
-int main(int argc, char *argv[])
-{
+
+int main(int argc, char *argv[]) {
   int limit = 1000;
   string interface = "wlx00c0cab05cec";
   if (argc > 2) {
     interface = argv[1];
     limit = atoi(argv[2]);
   } else {
-    cerr << "Format error: " << "\n";
+    cerr << "Format error: "
+         << "\n";
     cerr << "Usage: sudo ./sniff-report <interface> <packet limit>";
     return 1;
   }
@@ -112,18 +62,23 @@ int main(int argc, char *argv[])
   config.set_rfmon(true);
   config.set_filter("type data or type mgt subtype probe-req or type mgt subtype beacon");
   Sniffer sniffer(interface, config);
-  
 
-  while (true) {
-    
+  bool running = true;
+  while (running) {
+
+    if (count >= limit) {
+      running = false;
+      break;
+    }
     try {
       Packet packet = sniffer.next_packet();
-    
-      //checking if packet has data we care about from parse_probe function
+
+      // checking if packet has data we care about from parse_probe function
       if (parse_probe(packet) != "") {
         cout << count << ": " << parse_probe(packet) << endl;
         count++;
       }
+
     } catch (...) {
     }
   }
--- setup.sh
+++ setup.sh
@@ -1,6 +1,25 @@
-#! /usr/bin/bash
-echo "In order to run, you need a monitor mode interface, please enter the name of the device below"
-read devname
-sudo ip link set $devname down
-sudo iw $devname set monitor none
-sudo ip link set $devname up
+#!/bin/bash
+
+# Get list of interfaces
+interfaces=($(ifconfig -a | grep -o '^[^ ]*:' | tr -d :))
+
+# Print interfaces
+echo "Available interfaces:"
+for i in "${!interfaces[@]}"; do
+  printf "%d) %s\n" "$((i+1))" "${interfaces[i]}"
+done
+
+# Ask user to select an interface
+read -p "Select an interface to put into monitor mode: " selected
+selected_interface=${interfaces[selected-1]}
+
+# Put interface into monitor mode
+sudo ifconfig $selected_interface down
+sudo iwconfig $selected_interface mode monitor
+sudo ifconfig $selected_interface up
+
+# Confirm that interface is in monitor mode
+iwconfig $selected_interface | grep Mode: | grep Monitor
+
+echo "Interface $selected_interface is now in monitor mode."
+
